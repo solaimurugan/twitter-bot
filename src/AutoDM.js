00d2b92f2ec3c073da5cc03b1fc159420cfc87d8
solaimurugan v <solaimurugan.v@gmail.com>
2018-07-30 00:40:35 +0530

    AutoDM.js
    Added new thanks message

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hi ${name} Thanks for following, #பொன்விழாத்தலைவர்கலைஞர் \n #கலைஞர் #mkstalin  😊😊 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
